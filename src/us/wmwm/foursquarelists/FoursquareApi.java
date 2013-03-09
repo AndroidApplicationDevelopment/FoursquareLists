070ca9696c065209a06dab1d6a07c90ba10de462
Gravener <gravener@BAM-024170-RG.(none)>
2013-03-09 11:44:31 -0500

    add fsq login fragment and fsq api

---
+++ src/us/wmwm/foursquarelists/FoursquareApi.java
@@ -0,0 +1,11 @@
+package us.wmwm.foursquarelists;
+
+public class FoursquareApi {
+
+	private static final String CLIENT_ID="MUSHT2OIZ1RD5Y2EECLK3PSY4Z3L3VEJGJFN2KBN5FTX1XFG";
+	
+	private static final String CLIENT_SECRET="YFA4VVV4A05POOIVOXNFR5TCNN2ONQ5YJKCBBUAXCEIWEA4M";
+	
+	
+	
+}
---
+++ src/us/wmwm/foursquarelists/fragments/LoginFragment.java
@@ -0,0 +1,26 @@
+package us.wmwm.foursquarelists.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+public class LoginFragment extends Fragment {
+
+	WebView webView;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+}
